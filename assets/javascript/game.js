61bdc121ea4a1fc40157c6a3cecc8a780124dd94
David Lopez <dlopez079@gmail.com>
2019-09-03 18:05:33 -0400

    Added the variables that I will need for the javascript.

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//Create the variables that are needed to execute the game. 
+
+//variable for random computer number
+var randomCPUnum;
+var wins;
+var losses;
+var redCrystalnum;
+var blueCrystalnum;
+var yellowCrystalnum;
+var greenCrystalnum;
+var totalScoreNum;
