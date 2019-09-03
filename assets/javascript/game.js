f416b88018a747b7ab12b6f06015b989dc871552
David Lopez <dlopez079@gmail.com>
2019-09-03 18:43:07 -0400

    Created the function to update display on HTML on javascript file.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,28 @@
 //Create the variables that are needed to execute the game. 
 
-//variable for random computer number
-var randomCPUnum;
-var wins;
-var losses;
-var redCrystalnum;
-var blueCrystalnum;
-var yellowCrystalnum;
-var greenCrystalnum;
+var wins = 0;
+var losses = 0;
+var redCrystalnum = 0;
+var blueCrystalnum = 0;
+var yellowCrystalnum = 0;
+var greenCrystalnum = 0;
 var totalScoreNum;
+
+//Create a function to update display on HTML
+function updateDisplay () {
+    document.querySelector("randomNum").innerHTML = randomCPUnum;
+    document.querySelector("#wins").innerHTML = wins;
+    document.querySelector("#losses").innerHTML = losses;
+    document.querySelector("#redCrystal").innerHTML = redCrystalnum;
+    document.querySelector("#blueCrystal").innerHTML = blueCrystalnum;
+    document.querySelector("#yellowCrystal").innerHTML = yellowCrystalnum;
+    document.querySelector("#greenCrystal").innerHTML = greenCrystalnum;
+    document.querySelector("#userScore").innerHTML = totalScoreNum;
+}
+
+
+//Generate function for random computer number. 
+function randomCPUnum() {
+    var x = Math.floor((Math.random() * 99) + 1);
+    document.getElementById("#randomNum").innerHTML = x;
+  }
