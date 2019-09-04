e3b51309ba2b2e5b53c1268e6bf2293a3299d1fc
David Lopez <dlopez079@gmail.com>
2019-09-03 23:43:18 -0400

    Removed code from Javascript file.  The code is incorrect and I don't understand it.  I will re-create the content in game.gs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,27 +2,10 @@
 
 var wins = 0;
 var losses = 0;
+var randomNumber;
 var redCrystalnum = 0;
 var blueCrystalnum = 0;
 var yellowCrystalnum = 0;
 var greenCrystalnum = 0;
 var totalScoreNum;
 
-//Create a function to update display on HTML
-function updateDisplay () {
-    document.querySelector("randomNum").innerHTML = randomCPUnum;
-    document.querySelector("#wins").innerHTML = wins;
-    document.querySelector("#losses").innerHTML = losses;
-    document.querySelector("#redCrystal").innerHTML = redCrystalnum;
-    document.querySelector("#blueCrystal").innerHTML = blueCrystalnum;
-    document.querySelector("#yellowCrystal").innerHTML = yellowCrystalnum;
-    document.querySelector("#greenCrystal").innerHTML = greenCrystalnum;
-    document.querySelector("#userScore").innerHTML = totalScoreNum;
-}
-
-
-//Generate function for random computer number. 
-function randomCPUnum() {
-    var x = Math.floor((Math.random() * 99) + 1);
-    document.getElementById("#randomNum").innerHTML = x;
-  }
